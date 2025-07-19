dfa8c1995e33c0d39eb61b7e815b568cfa3271c2
Enock Ngetich <ngetichenock32@gmail.com>
2025-07-19 22:45:49 +0300

    Update

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState, ReactNode } from "react"
-import axios from "../../api/axios"
+import axios from "../api/axios"
 
 interface User {
   id: string
